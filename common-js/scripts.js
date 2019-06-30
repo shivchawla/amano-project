7ddabd0476d79501395f850f6fe24655b26b0395
shivchawla <shivchawla2001@gmail.com>
2019-06-30 20:26:17 +0530

    Initial Commit - Coming soon!

---
+++ common-js/scripts.js
@@ -0,0 +1,284 @@
+
+	
+(function ($) {
+
+    "use strict";
+
+	// JQUERY LIGHT BOX
+	
+	if ( $.isFunction($.fn.fluidbox) ) {
+		$('a').fluidbox();
+	}
+	
+	
+	$('a[href="#"]').on('click', function(event){
+		return;
+	});
+	
+	
+	$('[data-nav-menu]').on('click', function(event){
+		
+		var $this = $(this),
+			visibleHeadArea = $this.data('nav-menu');
+		
+		$(visibleHeadArea).toggleClass('visible');
+		
+	});
+	
+	
+	var winWidth = $(window).width();
+	dropdownMenu(winWidth);
+	
+	$(window).on('resize', function(){
+		dropdownMenu(winWidth);
+		
+	});
+	
+	// Circular Progress Bar
+	
+	var isAnimated = false;
+
+	/*==================================================================
+    [ Simple slide100 ]*/
+
+    $('.right-section').each(function(){
+        var delay = 4000;
+        var speed = 0;
+        var itemSlide = $(this).find('.simpleslide100-item');
+        var nowSlide = 0;
+
+        $(itemSlide).hide();
+        $(itemSlide[nowSlide]).show();
+        nowSlide++;
+        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+
+        setInterval(function(){
+            $(itemSlide).fadeOut(speed);
+            $(itemSlide[nowSlide]).fadeIn(speed);
+            nowSlide++;
+            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+        },delay);
+    });
+
+	// Give the parameter a variable name
+	var dynamicContent = getParameterByName('signup');
+
+	console.log("My name is Khan");
+	console.log(dynamicContent);
+	console.log(dynamicContent == '1');
+
+ 	$(document).ready(function() {
+
+		// Check if the URL parameter is 1
+		if (dynamicContent == '1') {
+			console.log("WTFFFF-1")
+			$('#success-signup').show();
+		} 
+		// Check if the URL parmeter is empty or not defined, display default content
+		else {
+			console.log("WTFFFF-2")
+			$('#default-content').show();
+		}
+	});
+	
+})(jQuery);
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function dropdownMenu(winWidth){
+	
+	if(winWidth > 767){
+		
+		$('.main-menu li.drop-down').on('mouseover', function(){
+			var $this = $(this),
+				menuAnchor = $this.children('a');
+				
+			menuAnchor.addClass('mouseover');
+			
+		}).on('mouseleave', function(){
+			var $this = $(this),
+				menuAnchor = $this.children('a');
+				
+			menuAnchor.removeClass('mouseover');
+		});
+		
+	} else{
+		
+		$('.main-menu li.drop-down > a').on('click', function(){
+			
+			if($(this).attr('href') == '#') return false;
+			if($(this).hasClass('mouseover')){ $(this).removeClass('mouseover'); }
+			else{ $(this).addClass('mouseover'); }
+			return false;
+		});
+	}
+	
+}
+
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
+
+function initMap() {
+
+	// Create a new StyledMapType object, passing it an array of styles,
+	// and the name to be displayed on the map type control.
+	var styledMapType = new google.maps.StyledMapType(
+	[
+		{
+			"featureType": "administrative",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": "-100"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": 65
+				},
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": "50"
+				},
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": "-100"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "all",
+			"stylers": [
+				{
+					"lightness": "30"
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "all",
+			"stylers": [
+				{
+					"lightness": "40"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"hue": "#ffff00"
+				},
+				{
+					"lightness": -25
+				},
+				{
+					"saturation": -97
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"lightness": -25
+				},
+				{
+					"saturation": -100
+				}
+			]
+		}
+	],
+		{name: 'Styled Map'});
+
+	// Create a map object, and include the MapTypeId to add
+	// to the map type control.
+
+	var uluru = {lat: 56.946285, lng: 24.105078};
+    var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: uluru
+    });
+		
+	var image = 'images/google-marker.png';
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map,
+		icon: image
+	});
+	//Associate the styled map with the MapTypeId and set it to display.
+	map.mapTypes.set('styled_map', styledMapType);
+	map.setMapTypeId('styled_map');
+}
